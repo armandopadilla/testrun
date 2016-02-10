c26eaab179bfdb377217893bef0744fcaaeff920
Armando Padilla <armando@crowdrise.com>
2016-02-10 11:03:59 -0800

    test

---
+++ app/src/test/java/wiredpanda/com/testrun/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package wiredpanda.com.testrun;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
